9e8696d08331643202b8c1887a5b826e0d46e2ef
James Ward <james@jamesward.org>
2012-08-07 11:07:55 -0600

    add Date header to results

--- app/controllers/RemoteAssets.scala
+++ app/controllers/RemoteAssets.scala
@@ -1,16 +1,27 @@
 package controllers
 
-import play.api.mvc.{AnyContent, Action}
-import play.api.mvc.Controller
+import play.api.mvc._
 import play.api.Play
 import play.api.Play.current
+import org.joda.time.format.{DateTimeFormat, DateTimeFormatter}
+import org.joda.time.DateTimeZone
+import scala.Some
 
 
 object RemoteAssets extends Controller {
 
+  private val timeZoneCode = "GMT"
+
+  private val df: DateTimeFormatter =
+    DateTimeFormat.forPattern("EEE, dd MMM yyyy HH:mm:ss '"+timeZoneCode+"'").withLocale(java.util.Locale.ENGLISH).withZone(DateTimeZone.forID(timeZoneCode))
+  
+  type ResultWithHeaders = Result { def withHeaders(headers: (String, String)*): Result }
+  
   def at(path: String, file: String): Action[AnyContent] = Action { request =>
-    val action: Action[AnyContent] = Assets.at(path, file)
-    action.apply(request)
+    val action = Assets.at(path, file)
+    val result = action.apply(request)
+    val resultWithHeaders = result.asInstanceOf[ResultWithHeaders]
+    resultWithHeaders.withHeaders(DATE -> df.print({new java.util.Date}.getTime))
   }
 
   def at(url: String) = {

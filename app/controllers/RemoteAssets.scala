d0f3750387dbbf7d0fe7f0f68648fa8aa94cbbf7
James Ward <james@jamesward.org>
2012-08-08 12:07:00 -0600

    article done

--- app/controllers/RemoteAssets.scala
+++ app/controllers/RemoteAssets.scala
@@ -14,27 +14,20 @@ object RemoteAssets extends Controller {
 
   private val df: DateTimeFormatter =
     DateTimeFormat.forPattern("EEE, dd MMM yyyy HH:mm:ss '"+timeZoneCode+"'").withLocale(java.util.Locale.ENGLISH).withZone(DateTimeZone.forID(timeZoneCode))
-  
+
   type ResultWithHeaders = Result { def withHeaders(headers: (String, String)*): Result }
-  
-  def at(path: String, file: String): Action[AnyContent] = Action { request =>
+
+  def getAsset(path: String, file: String): Action[AnyContent] = Action { request =>
     val action = Assets.at(path, file)
     val result = action.apply(request)
     val resultWithHeaders = result.asInstanceOf[ResultWithHeaders]
     resultWithHeaders.withHeaders(DATE -> df.print({new java.util.Date}.getTime))
   }
 
-  def at(url: String) = {
-    Play.configuration.getString("contentURL") match {
-      case Some(contentURL) => contentURL + controllers.routes.RemoteAssets.at(url).url
-      case None => controllers.routes.RemoteAssets.at(url)
-    }
-  }
-
-  def getContentURL() = {
-    Play.configuration.getString("contentURL") match {
-      case Some(contentURL) => contentURL
-      case None => ""
+  def getUrl(file: String) = {
+    Play.configuration.getString("contenturl") match {
+      case Some(contentUrl) => contentUrl + controllers.routes.RemoteAssets.getAsset(file).url
+      case None => controllers.routes.RemoteAssets.getAsset(file)
     }
   }
 
